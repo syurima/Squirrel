8c06da70e9e6e06f44ab404b4f85ac2f39589963
syurima <natka.dys@gmail.com>
2026-05-31 12:18:07 +0200

    move more functions

--- srcs/internal/mysql/srcs/mutator.cpp
+++ srcs/internal/mysql/srcs/mutator.cpp
@@ -191,50 +191,6 @@ bool Mutator::lucky_enough_to_be_mutated(unsigned int mutated_times) {
   return false;
 }
 
-pair<string, string> Mutator::get_data_2d_by_type(DATATYPE type1,
-                                                  DATATYPE type2) {
-  pair<string, string> res("", "");
-  auto size = data_library_2d_[type1].size();
-
-  if (size == 0) return res;
-  auto rint = get_rand_int(size);
-
-  int counter = 0;
-  for (auto &i : data_library_2d_[type1]) {
-    if (counter++ == rint) {
-      return std::make_pair(i.first,
-                pick_random_element(i.second[type2]));
-    }
-  }
-  return res;
-}
-
-IR *Mutator::generate_ir_by_type(IRTYPE type) {
-  auto ast_node = generate_ast_node_by_type(type);
-  ast_node->generate();
-  vector<IR *> tmp_vector;
-  ast_node->translate(tmp_vector);
-  assert(tmp_vector.size());
-
-  return tmp_vector[tmp_vector.size() - 1];
-}
-
-IR *Mutator::get_ir_from_library(IRTYPE type) {
-  const int generate_prop = 1;
-  const int threshold = 0;
-  static IR *empty_ir = new IR(kStringLiteral, "");
-#ifdef USEGENERATE
-  if (ir_library_[type].empty() == true ||
-      (get_rand_int(400) == 0 && type != kUnknown)) {
-    auto ir = generate_ir_by_type(type);
-    add_ir_to_library_no_deepcopy(ir);
-    return ir;
-  }
-#endif
-  if (ir_library_[type].empty()) return empty_ir;
-  return pick_random_element(ir_library_[type]);
-}
-
 // Implementations for many Mutator methods were moved to
 // srcs/internal/common/srcs/mutator_core.cpp to avoid duplication across
 // dialects. See that file for the shared definitions.
