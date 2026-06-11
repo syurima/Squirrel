7e1f2d3c9e3b5c54ccf3a9a304c456c4acf541ad
syurima <natka.dys@gmail.com>
2026-06-11 18:57:00 +0200

    basic fix for run jobs

--- wcss_run.sh
+++ wcss_run.sh
@@ -2,12 +2,9 @@
 #SBATCH -N1                                   # Liczba węzłów
 #SBATCH -c5                                   # Liczba rdzeni cpu  
 #SBATCH --mem=16gb                            # Ilość pamięci RAM
-#SBATCH --time=${2:-00:20:00}                 # Limit czasowy na zadanie (nadpisywany przez argument, domyślnie 20 min)
 #SBATCH --job-name=squirrel_${DBMS}_run       # Nazwa zadania
 #SBATCH -p lem-gpu-short                      # Nazwa partycji
 #SBATCH --gres=gpu:hopper:1                   # Potrzebne zasoby GPU
-
-set -euo pipefail
 
 # This script is used in the GitHub Actions workflow to run the fuzzing job on WCSS.
 
@@ -22,5 +19,5 @@ fi
 
 echo "=== Running fuzzing job with Apptainer image ${APPTAINER_IMAGE} ==="
 # --nv is needed to enable GPU support in the container. don't delete it!!
-apptainer exec --nv $APPTAINER_IMAGE
+apptainer run --nv $APPTAINER_IMAGE
 echo "=== Fuzzing job finished ==="
